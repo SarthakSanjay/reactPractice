bf5ece0607134ccdb9c4170e832d0359236e0c7d
Sarthak Sanjay <sarthaksanjaycoll21@gmail.com>
2023-02-15 01:51:41 +0530

    ic

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -8,11 +8,12 @@ const Tour = (props) => {
     const [paragraph , setParagraph] = useState(first30words)
     const [readStatus , setReadStatus] = useState("Read More")
     const handleCLick = () => {
+        
     }
     const readMore = () => {
         setParagraph(props.info)
         setReadStatus("Read Less")
-
+        
     }
     const readLess = () =>{
         setParagraph(first30words)
@@ -26,14 +27,15 @@ const Tour = (props) => {
         <h4>{props.name}</h4>
         <h4 className='price'>$ {props.price}</h4>
         </div>
-        <p>{paragraph}...<span onClick={readMore}>{readStatus}</span></p>
+        <p>{paragraph}...{readStatus ==="Read More" ? <span onClick={readMore}>{readStatus}</span> : <span onClick={readLess}>{readStatus}</span>}
+        </p>
         <button onClick={handleCLick}>Not Interested</button>
     </Container>
   )
 }
 
 const Container = styled.div`
-height:400px;
+min-height:400px;
 width: 600px;
 background-color: white;
 margin: 50px 0px;
@@ -57,6 +59,16 @@ p{
     span{
         color: #0fbaff;
     }
+}
+
+button{
+    background-color: white;
+    border: 2px solid red;
+    color: red;
+    border-radius: 5px;
+    width:200px;
+    height: 30px;
+    margin: 0 180px;
 }
 
 `
